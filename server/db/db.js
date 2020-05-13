ad5a2b71994a81e622d9e5cbf3a39ca826896cae
zhangjinxing <zhangjx@minstone.com.cn>
2020-05-13 10:04:07 +0800

    修改mysql连接方式，采用连接池方式，需要用到数据库再去连接

--- server/db/db.js
+++ server/db/db.js
@@ -1,17 +1,11 @@
 const mysql = require('mysql');
 
 // 创建连接池
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
     host: "123.56.119.87",
     user: "root",
     password: "199336",
-    database: "study"
+    database: "study",
+    connectionLimit : 10,
 });
-connection.connect(function(err) {
-    if (err) {
-        console.log('err connecting:' + err);
-        return
-    };
-    console.log('mysql连接成功!');
-});
-module.exports = connection;
+module.exports = pool;
--- server/db/index.js
+++ server/db/index.js
@@ -3,15 +3,17 @@ const db = require('./db');
 // 用promise封装一下一个高阶函数
 
 const querySql = (db) => (sql) => new Promise(async (resolve, reject) => {
+    db.getConnection((err, connection) => {
+        connection.query(sql, function(error, results){
+            if (error) {
+                reject(error)
+                return false;
+            };
+            resolve(results);
+            connection.release();
+        });
+    })  
     
-    db.query(sql, function(error, results){
-        if (error) {
-            reject(error)
-            return false;
-        };
-        resolve(results);
-        
-    });
 });
 
 module.exports = querySql(db);

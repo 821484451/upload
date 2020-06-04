b10edec87398081a9350ea813779c7bf08cc8275
zhangjinxing <821484451@qq.com>
2020-06-04 10:59:26 +0800

    修改ip

--- server/db/db.js
+++ server/db/db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 
 // 创建连接池
 const pool = mysql.createPool({
-    host: "123.56.119.87",
+    host: "localhost",
     user: "root",
     password: "199336",
     database: "study",
-    connectionLimit : 10,
+    connectionLimit: 10,
 });
 module.exports = pool;
